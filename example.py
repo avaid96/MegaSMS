5e7129121f9d8566a6242a58ff82b4bf9cd724b2
avaid96 <avaid1996@gmail.com>
2015-12-18 17:09:49 +0530

    personalized

--- example.py
+++ example.py
@@ -11,8 +11,11 @@ account_sid = os.getenv('MY_ACCOUNT_SID')
 auth_token  = os.getenv('MY_AUTH_TOKEN')
 client = TwilioRestClient(account_sid, auth_token)
 for row in csvnum:
-	print row
-	message = client.messages.create(body="Twilio working, buy a number",
-    	to=row,    # Replace with your phone number or text me if you really want to
+	# print row[0]
+	name=row[1]
+	text="Hi,"+name
+	message = client.messages.create(body=text,
+    	to=row[0],    # Replace with your phone number or text me if you really want to
     	from_="+15005550006") # Replace with your Twilio number
 	print message.sid
+
